dfe12944a382d2f675951065f3ae166a8fadc768
nikhilaharsha <kshvr1699@gmail.com>
2023-05-19 11:21:07 -0400

    Create setup.cfg

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='CS410_FlyEM_Segmentation_Pipeline',
+    version='1.0',
+    license='MIT',
+    author="Sai Harshavardhan Reddy Kona",
+    author_email='s.kona001@umb.edu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/kshvr16/CS410_FlyEM_TestPyPI_V2',
+    keywords='Testing PyPi deployment',
+    install_requires=[
+        'numpy',
+        'mahotas',
+        'matplotlib',
+        'scikit-image'
+      ],
+
+)
